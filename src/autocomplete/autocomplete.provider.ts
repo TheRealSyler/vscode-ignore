b28d9a14ff42bebb271dedddd5ae0f6674b51f7c
syler <hileo77@gmail.com>
2020-01-24 20:19:56 +0100

    Release 0.0.2

--- src/autocomplete/autocomplete.provider.ts
+++ src/autocomplete/autocomplete.provider.ts
@@ -3,30 +3,30 @@ import {
   CompletionItemProvider,
   TextDocument,
   Position,
-  CancellationToken,
   CompletionItem,
   Range,
   CompletionItemKind
 } from 'vscode';
 import { resolve, basename } from 'path';
-import { promises, statSync } from 'fs';
+import { promises, statSync, existsSync } from 'fs';
 export class GitIgnoreCompletions implements CompletionItemProvider {
   context: ExtensionContext;
   constructor(context: ExtensionContext) {
     this.context = context;
   }
-  async provideCompletionItems(
-    document: TextDocument,
-    position: Position,
-    token: CancellationToken
-  ) {
+  async provideCompletionItems(document: TextDocument, position: Position) {
     const start = new Position(position.line, 0);
     const range = new Range(start, position);
     const currentWord = document.getText(range).trim();
-    const tempDir = resolve(document.uri.path, '../', currentWord);
-    const dir = statSync(tempDir).isDirectory() ? tempDir : resolve(document.uri.path, '../');
+
+    const path = resolve(document.uri.path, '../', currentWord.replace(/^\//, ''));
+    if (!existsSync(path)) {
+      return [];
+    }
+    const dir = statSync(path).isDirectory() ? path : resolve(document.uri.path, '../');
 
     const files = await promises.readdir(dir);
+
     const completions: CompletionItem[] = [];
 
     for (let i = 0; i < files.length; i++) {
@@ -34,20 +34,25 @@ export class GitIgnoreCompletions implements CompletionItemProvider {
       if (fileName === basename(document.fileName)) {
         continue;
       }
-      const stats = statSync(resolve(document.uri.path, '../', fileName));
-      const isDir = stats.isDirectory();
-      const Item = new CompletionItem(
-        fileName,
-        isDir ? CompletionItemKind.Folder : CompletionItemKind.File
-      );
-      Item.insertText = isDir ? `${fileName}/` : fileName;
-      Item.command = isDir
-        ? {
-            title: 'Trigger Completion',
-            command: 'editor.action.triggerSuggest'
-          }
-        : undefined;
-      completions.push(Item);
+      try {
+        const stats = statSync(resolve(dir, fileName));
+        const isDir = stats.isDirectory();
+        const Item = new CompletionItem(
+          fileName,
+          isDir ? CompletionItemKind.Folder : CompletionItemKind.File
+        );
+        const insertText = isDir ? `${fileName}/` : fileName;
+        Item.insertText = currentWord.endsWith('.') ? `/${insertText}` : insertText;
+        Item.command = isDir
+          ? {
+              title: 'Trigger Completion',
+              command: 'editor.action.triggerSuggest'
+            }
+          : undefined;
+        completions.push(Item);
+      } catch {
+        console.log(`[ignore]: Autocomplete, ${fileName} Failed`);
+      }
     }
 
     return completions;
