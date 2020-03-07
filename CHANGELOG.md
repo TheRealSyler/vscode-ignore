8e1b8123f7fa165320633f4b6d8881fbae4691e5
syler <hileo77@gmail.com>
2020-03-07 17:57:18 +0100

    fix path completion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-| Date      | Message             |
-| --------- | ------------------- |
-| 24.1.2020 | Initial Release     |
-| 5.2.2020  | fix path completion |
+| Date      | Message                     |
+| --------- | --------------------------- |
+| 7.3.2020  | fix path completion (again) |
+| 5.2.2020  | fix path completion         |
+| 24.1.2020 | Initial Release             |
--- src/autocomplete/autocomplete.provider.ts
+++ src/autocomplete/autocomplete.provider.ts
@@ -7,7 +7,7 @@ import {
   Range,
   CompletionItemKind
 } from 'vscode';
-import { resolve, basename, normalize, join } from 'path';
+import { resolve, basename } from 'path';
 import { promises, statSync, existsSync } from 'fs';
 export class GitIgnoreCompletions implements CompletionItemProvider {
   context: ExtensionContext;
@@ -17,44 +17,45 @@ export class GitIgnoreCompletions implements CompletionItemProvider {
   async provideCompletionItems(document: TextDocument, position: Position) {
     const start = new Position(position.line, 0);
     const range = new Range(start, position);
-    const currentWord = document
-      .getText(range)
-      .replace(/^(\/|\.)/, '')
-      .trim();
+    const rawCurrentWord = document.getText(range).trim();
+    const currentWord = rawCurrentWord.replace(/^(\/|\*)/, '');
 
-    const path = resolve(document.uri.path, '../', currentWord, currentWord ? '../' : '');
-    console.log(path);
+    const path = resolve(document.uri.path, '../', currentWord);
+
     if (!existsSync(path)) {
       console.log("[vscode-ignore]: Exited autocomplete, path doesn't exist.");
       return [];
     }
-    const dir = statSync(path).isDirectory() ? path : resolve(document.uri.path, '../');
-    const files = await promises.readdir(dir);
+
+    const files = await promises.readdir(path);
     const completions: CompletionItem[] = [];
 
     for (let i = 0; i < files.length; i++) {
       const fileName = files[i];
-      if (fileName === basename(document.fileName) || fileName.startsWith('.')) {
+      if (fileName === basename(document.fileName)) {
         continue;
       }
       try {
-        const stats = statSync(resolve(dir, fileName));
+        const stats = statSync(resolve(path, fileName));
         const isDir = stats.isDirectory();
-        const Item = new CompletionItem(
+        const item = new CompletionItem(
           fileName,
           isDir ? CompletionItemKind.Folder : CompletionItemKind.File
         );
-        const insertText = isDir ? `${fileName}/` : fileName;
-        Item.insertText = currentWord.endsWith('.') ? `/${insertText}` : insertText;
-        Item.command = isDir
+        const insertText = (isDir ? `${fileName}/` : fileName).replace(
+          new RegExp(`^${rawCurrentWord.replace(/\./, '\\.')}`),
+          ''
+        );
+        item.insertText = currentWord.endsWith('.') ? `/${insertText}` : insertText;
+        item.command = isDir
           ? {
               title: 'Trigger Completion',
               command: 'editor.action.triggerSuggest'
             }
           : undefined;
-        completions.push(Item);
-      } catch {
-        console.log(`[ignore]: Autocomplete, ${fileName} Failed`);
+        completions.push(item);
+      } catch (e) {
+        console.log(`[ignore]: Autocomplete, ${fileName} Failed`, e);
       }
     }
     return completions;
